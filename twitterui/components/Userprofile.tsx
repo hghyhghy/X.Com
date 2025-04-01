e98ebd05c0284a9b0f46243a59a5fdf1129e129d
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-04-01 13:14:02 +0530

    updated  (#6) the sign out functionality in the userprofile page  ..

--- twitterui/components/Userprofile.tsx
+++ twitterui/components/Userprofile.tsx
@@ -1,14 +1,17 @@
 // components/Userprofile.tsx
+'use client'
+import Cookies from "js-cookie";
+import { useState } from "react";
 import { ReactNode } from "react";
 import Image from "next/image";
 import { FaCircleUser } from "react-icons/fa6";
 import { BsThreeDots } from "react-icons/bs";
+import { useRouter } from "next/navigation";
 
 interface User {
     username: string;
     email: string;
     avatarUrl?: ReactNode;
-    threedots?:ReactNode
 }
 
 interface ProfileProps {
@@ -16,6 +19,12 @@ interface ProfileProps {
 }
 
 const Userprofile = ({ user }: ProfileProps) => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const router=  useRouter()
+    const handleSignOut=()=>{
+        Cookies.remove("token")
+        router.push("/login")
+    }
     if (!user) {
         return <p>Loading...</p>;  // Show loading state if no user data
     }
@@ -43,21 +52,22 @@ const Userprofile = ({ user }: ProfileProps) => {
                     <p><strong></strong> {user.username}</p>
                     <p className=" text-gray-600">{user.email}</p>
                     </div>
-                    <div>
+                    <button onClick={() => setIsModalOpen(!isModalOpen)} className="relative mb-5 cursor-pointer">
+            <BsThreeDots size={24} className="text-white" />
+          </button>
 
-                    {typeof user.threedots === 'string' ? (
-                        <Image
-                        src={user.threedots}
-                        alt="User threedots"
-                        className="w-full h-full   text-black mt-5 p-4 "
-                        width={48}
-                        height={48}
-                        />
-                    ) : (
-                        user.threedots || <BsThreeDots  size={40}  />  // Fallback to FaUserCircle if avatarUrl is not provided
-                    )}
-                    </div>
                 </div>
+
+                {isModalOpen && (
+        <div className="absolute  left-32 top-[38rem] mt-2 w-[18rem] bg-gradient-to-r from-gray-800 via-gray-900 to-black border border-gray-600 rounded-lg shadow-xl p-4 z-50 transition-all duration-300 ease-in-out transform scale-100 opacity-100">
+        <button
+          onClick={handleSignOut}
+          className="block w-full text-left px-6 py-3 text-white rounded-lg transition-colors duration-200 cursor-pointer"
+        >
+          Sign Out
+        </button>
+      </div>
+                )}
             </div>
         </div>
     );
