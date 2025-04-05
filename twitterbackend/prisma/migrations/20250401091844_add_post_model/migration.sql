baeef9106cdd35e12b8d3f92e2cc368efed09962
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-04-05 10:53:29 +0530

    added new  (#26) migrations to the prisma server

---
+++ twitterbackend/prisma/migrations/20250401091844_add_post_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `topic` VARCHAR(191) NOT NULL,
+    `links` VARCHAR(191) NULL,
+    `hashtags` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
