b4e157226bec382b2038888e223cfb63026397a1
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-04-10 13:08:04 +0530

    updated (#64)  to add the post modeule in the app.module.ts file

--- twitterbackend/src/app.module.ts
+++ twitterbackend/src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
+import { GeminiService } from './gemini/gemini.service';
+import { ExploreModule } from './explore/explore.module';
+import { TrendingGeminiService } from './trending-gemini/trending-gemini.service';
+import { TrendingModule } from './trending/trending.module';
+import { NewsGeminiService } from './news-gemini/news-gemini.service';
+import { NewsModule } from './news/news.module';
+import { TechGeminiService } from './tech-gemini/tech-gemini.service';
+import { TechnologyModule } from './technology/technology.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [ AuthModule],
+  imports: [ AuthModule, ExploreModule, TrendingModule, NewsModule, TechnologyModule, PostsModule],
   controllers: [AppController],
-  providers: [AppService,PrismaService],
+  providers: [AppService,PrismaService, GeminiService, TrendingGeminiService, NewsGeminiService, TechGeminiService],
 })
 export class AppModule {}
