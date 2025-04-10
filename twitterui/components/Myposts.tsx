e5871e6c68bfad6592e2699add29f78eadf3c25b
subham sarkar <subhamsarkar.aeie2021@nsec.ac.in>
2025-04-10 09:19:24 +0530

    added (#59) myposts section  to show the posts in the frontend

---
+++ twitterui/components/Myposts.tsx
@@ -0,0 +1,188 @@
+'use client';
+import { useState,useEffect } from "react";
+import Cookies from "js-cookie";
+import Image from "next/image";
+import axios from "axios";
+import { MdVerified } from "react-icons/md";
+import { FaRegCircleUser } from "react-icons/fa6";
+type Post={
+    id: number;
+    user:{
+        username:string,
+        email:string
+    },
+    topic: string;
+    content: string;
+    media: { url: string; type: 'image' | 'video' }[];
+    hashtags: string[];
+    links: string[];
+    createdAt:string
+}
+
+
+export default function  Myposts(){
+    const api = 'http://localhost:3001/posts'; 
+
+    const token  = Cookies.get('token')
+    const [posts, setPosts] = useState <Post[]>([])
+    useEffect(()=>{
+        fetchPosts()
+    },[])
+
+    const  fetchPosts = async() => {
+
+        try {
+            
+            const   resposne  =  await axios.get(`${api}/all`, {
+                headers:{
+                    Authorization:`Bearer ${token}`
+                },
+                withCredentials:true
+            })
+            const  arraypost  =   resposne.data.posts.map((post:any) => ({
+                ...post,
+                hashtags:typeof post.hashtags === 'string'? JSON.parse(post.hashtags):post.hashtags,
+                links:typeof post.links === 'string'? JSON.parse(post.links):post.links
+
+            }))
+
+            setPosts(arraypost.reverse())
+        } catch (error) {
+            console.log('Failed to fetch posts',error)
+        }
+    }
+
+    return(
+                    <div className=" min-h-screen relative">
+                             <div className="absolute -left-0 top-0 bottom-0 w-px bg-gray-600 min-h-screen " />
+                             <div className="absolute -right-0 top-0 bottom-0 w-px bg-gray-600 min-h-screen " />
+
+      <div className="max-h-screen overflow-y-auto px-4">
+        
+                        {posts.map((post) => (
+        
+                            <div
+                            key={post.id}
+                            className="p-8  rounded shadow  border-b-2 border-gray-700 "
+                            >  
+
+                            <div className=' flex flex-row gap-1 '>
+                                <div className=' text-gray-300  font-bold  uppercase flex  flex-row'>
+                                <FaRegCircleUser className="  text-3xl relative -left-7 -mr-6"  /> {post.user.username} <MdVerified  className=' mt-[0.1rem] ml-1 text-blue-500 text-lg' /> 
+                                </div>
+                                <div className=' text-gray-500'>
+                                    @{post.user.email}
+                                </div>
+                                <div className=' text-gray-500 ml-1'>
+                                <div className=' mb-2'>
+                                .{new Date(post.createdAt).toISOString().split('T')[0]}
+                                </div>
+                                </div>
+                            </div>
+        
+                            <div className=' flex flex-col gap-2'>
+                            <div className="text-sm text-gray-100  flex flex-col  gap-2">
+                                <div className=" text-[1rem]">
+
+                                    {post.topic}
+                                </div>
+                                <div className=" text-[1rem] -mt-3 mb-2">
+
+                                    {post.content}
+                                </div>
+                                </div>
+                                <div className=" flex flex-row">
+
+        
+                                {Array.isArray(post.hashtags) &&    
+                                    post.hashtags.map((tag, index) => (
+                                        <span key={index} className="text-blue-600 text-lg -mt-4 ">
+                                        {tag}{','}
+                                        </span>
+                                    ))
+                                }
+                                </div>
+        
+                                {post.links && post.links.length > 0 && (
+        
+                                        <div
+                                        className="text-sm text-blue-500 underline space-y-1 mb-2"
+                                        >
+                                            {post.links.map((link,idx) => (
+        
+                                                <a 
+                                                key={idx}
+                                                href={link}
+                                                target="_blank" rel="noopener noreferrer"
+                                                className='  border-none outline-none'
+                                                >
+                                                    {link}
+                                                </a>
+                                            ))}
+        
+                                        </div>
+                                )}
+                            </div>
+        
+        
+                                <div
+                                className="flex flex-wrap gap-2 mt-2"
+                                >
+                                    {post.media.map((m,i) =>  
+                                    m.type === 'video'?(
+                                        <div
+                                        key={i}
+                                        className=' w-full'
+                                        >
+                                            <video
+                                            controls
+                                            className=' rounded w-full max-h-60'
+                                            >
+                                                <source src={m.url} />
+                                            </video>
+
+        
+                                        </div>
+                                    ) : (
+        
+                                        <div
+                                        key={i}
+                                        className=' relative'
+                                        >
+                                            <Image
+                                            src={m.url}
+                                            height={400}
+                                            width={550}
+                                            alt='media'
+                                            className=' object-cover rounded-xl h-[20rem] border border-gray-700'
+                                            />
+        
+        
+                                        </div>
+                                    )
+                                    )}
+        
+                                </div>
+        
+                                <div
+                                className=' mt-2'
+                                >
+
+        
+                                </div>
+{/*         
+                                <button
+                                onClick={() => handleDeletePost(post.id)}
+                                className="text-red-500 mt-2 text-sm"
+                                >
+                                    Delete Post
+                                </button> */}
+        
+                            </div>
+                        ))}
+      </div>
+
+                    </div>
+    )
+
+}
